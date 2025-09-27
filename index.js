99894bd34b8c428f164036648936fa6274fefc9b
Miguel Higuita <higuita1900@gmail.com>
2025-09-27 13:30:55 -0500

    refactor(barrel): remove unused code

--- index.js
+++ index.js
@@ -3,13 +3,3 @@ import "./components/pages/Home-page.js";
 import "./components/pages/Projects-page.js";
 import "./components/pages/About-page.js";
 import "./components/layout/Menu.js";
-
-window.app = {
-  showSection: function (seccionId) {
-    document.querySelectorAll(".component").forEach((sec) => {
-      sec.classList.remove("active");
-    });
-
-    document.getElementById(seccionId).classList.add("active");
-  },
-};
