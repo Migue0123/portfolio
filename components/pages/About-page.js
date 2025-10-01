c2eb25a29107cd74c541269ebd231db2330d46d2
Miguel Higuita <higuita1900@gmail.com>
2025-10-01 18:24:28 -0500

    style(about): add responsive breakpoint for mobile

--- components/pages/About-page.js
+++ components/pages/About-page.js
@@ -11,9 +11,15 @@ class About extends HTMLElement {
     return /* css */ `
       .content {
         display: flex;
-        -moz-column-gap: 2rem;
-            column-gap: 2rem;
+        flex-direction: row-reverse;
+        gap: 2rem;
         align-items: center;
+
+        .title {
+          display: inline-block;
+          text-wrap: balance;
+          text-align: center;
+        }
       }
 
       span {
@@ -33,6 +39,12 @@ class About extends HTMLElement {
         flex-direction: column;
         align-items: center;
       }
+
+      @media (width <= 768px) {
+        .content {
+          flex-direction: column;
+        }
+      }
     `;
   }
 
@@ -47,15 +59,15 @@ class About extends HTMLElement {
         ${About.styles}
       </style>
       <div class="content">
-        <article>
-          <h1>Soy Miguel Higuita, desarrollador web.</h1>
-          <p>Soy <span>desarrollador web con 4 años de experiencia</span> creando soluciones digitales robustas y escalables. Mi especialidad es el desarrollo front-end, donde construyo interfaces de usuario intuitivas y responsivas. Mi objetivo es transformar ideas complejas en código limpio, mantenible y de alto rendimiento.</p>
-          <p>Mi compromiso con la calidad se refleja en la atención al detalle. Esta minuciosidad me permite crear una experiencia de usuario fluida, coherente y entregar productos pulidos. <span>Considero que los pequeños detalles son los que marcan la diferencia entre un buen producto y uno que realmente destaque.</span></p>
-          <p>Mi motivación principal se centra en lo esencial: colaborar para construir productos digitales de calidad que sean útiles, eficientes y ofrezcan una experiencia excepcional al usuario. <span>Busco que cada aspecto técnico, por complejo que sea, se traduzca en simplicidad y valor para el cliente.</span></p>
-        </article>
         <article class="me">
           <optimized-image src="miguel-higuita" alt="Miguel Higuita"></optimized-image>
           <social-buttons></social-buttons>
+        </article>
+        <article>
+          <h1 class="title">Soy Miguel Higuita, desarrollador web.</h1>
+          <p>Soy <span>desarrollador web con 4 años de experiencia</span> creando soluciones digitales robustas y escalables. Mi especialidad es el desarrollo front-end, donde construyo interfaces de usuario intuitivas y responsivas. Mi objetivo es transformar ideas complejas en código limpio, mantenible y de alto rendimiento.</p>
+          <p>Mi compromiso con la calidad se refleja en la atención al detalle. Esta minuciosidad me permite crear una experiencia de usuario fluida, coherente y entregar productos pulidos. <span>Considero que los pequeños detalles son los que marcan la diferencia entre un buen producto y uno que realmente destaque.</span></p>
+          <p>Mi motivación principal se centra en lo esencial: colaborar para construir productos digitales de calidad que sean útiles, eficientes y ofrezcan una experiencia excepcional al usuario. <span>Busco que cada aspecto técnico, por complejo que sea, se traduzca en simplicidad y valor para el cliente.</span></p>
         </article>
       </div>
     `;
